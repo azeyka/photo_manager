ce0e3ce280871e31009724e0f2a7cd3c5833d6ce
Elena <www.azeyka@mail.ru>
2020-01-11 21:40:26 +0300

    Update PhotoManager.jsx

--- src/components/PhotoManager.jsx
+++ src/components/PhotoManager.jsx
@@ -1,18 +1,35 @@
-import React, { useState } from 'react'
-import AddPhoto from './AddPhoto';
-import PhotosListing from './PhotosListing';
-
+import React, { useState } from "react";
+import AddPhoto from "./AddPhoto";
+import PhotosListing from "./PhotosListing";
+import Photo from "./Photo";
 
 function PhotoManager() {
-    const [files, setFiles] = useState([]);
+  const [files, setFiles] = useState([]);
+  const [isPhotoOpened, setisPhotoOpened] = useState(false);
+  const [openedPhoto, setopenedPhoto] = useState({});
+  const [isAddTextShown, setisAddTextShown] = useState(true);
 
-    return (
-        <React.Fragment>
-            <AddPhoto setFiles={setFiles}/>
-            <PhotosListing files={files} setFiles={setFiles}/>
-        </React.Fragment>
-    )
-};
+  return (
+    <React.Fragment>
+      <AddPhoto
+        setFiles={setFiles}
+        isAddTextShown={isAddTextShown}
+        setisAddTextShown={setisAddTextShown}
+      />
+      <PhotosListing
+        files={files}
+        setFiles={setFiles}
+        setisPhotoOpened={setisPhotoOpened}
+        setopenedPhoto={setopenedPhoto}
+        setisAddTextShown={setisAddTextShown}
+      />
+      <Photo
+        isVisible={isPhotoOpened}
+        file={openedPhoto}
+        setisPhotoOpened={setisPhotoOpened}
+      />
+    </React.Fragment>
+  );
+}
 
-export default PhotoManager
-
+export default PhotoManager;
