8e2bc42d4742c7be76c73c6276f7551024f945f8
Elena <www.azeyka@mail.ru>
2020-01-11 21:40:29 +0300

    Update AddPhoto.jsx

--- src/components/AddPhoto.jsx
+++ src/components/AddPhoto.jsx
@@ -1,34 +1,56 @@
-import React, { useRef } from 'react';
-import nanoid from 'nanoid';
-import PropTypes from 'prop-types';
-
+import React, { useRef } from "react";
+import nanoid from "nanoid";
+import PropTypes from "prop-types";
 
 function AddPhoto(props) {
-    const {setFiles} = props;
-    const fileRef = useRef();
+  const { setFiles, isAddTextShown, setisAddTextShown } = props;
+  const fileRef = useRef();
 
-    const handleChange = (event) => {
-        event.preventDefault();
-        const newFiles = [];
-        for (let file of fileRef.current.files) {
-            if (file.type.includes('image')) {
-                newFiles.push({id: nanoid(), name: file.name, url: URL.createObjectURL(file)})
-            };
-        };
-        setFiles(prevFiles => [...prevFiles, ...newFiles]);
+  const handleChange = event => {
+    event.preventDefault();
+    const newFiles = [];
+    for (let file of fileRef.current.files) {
+      if (file.type.includes("image")) {
+        newFiles.push({
+          id: nanoid(),
+          name: file.name,
+          url: URL.createObjectURL(file)
+        });
+      }
     }
+    setFiles(prevFiles => [...prevFiles, ...newFiles]);
+    setisAddTextShown(false);
+  };
 
-    return (
-        <div>
-            <input name="files" type="file" ref={fileRef} onChange={handleChange} accept="image/*" multiple hidden/>
-            <button onClick={() => fileRef.current.click()} id='select'>Click to select</button>
-        </div>
-    )
-};
+  return (
+    <>
+      <input
+        name="files"
+        type="file"
+        ref={fileRef}
+        onChange={handleChange}
+        accept="image/*"
+        multiple
+        hidden
+      />
+      <button onClick={() => fileRef.current.click()} id="select">
+        {isAddTextShown ? (
+          <p>
+            <i className="material-icons">add_circle_outline</i>
+            <span> Кликните в пустое поле чтобы добавить изображение</span>
+          </p>
+        ) : (
+          ""
+        )}
+      </button>
+    </>
+  );
+}
 
 AddPhoto.propTypes = {
-    setFiles: PropTypes.func
+  setFiles: PropTypes.func,
+  isAddTextShown: PropTypes.bool,
+  setisAddTextShown: PropTypes.func
 };
 
 export default AddPhoto;
-
